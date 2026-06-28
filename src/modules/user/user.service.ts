38257867edfc338711082a03a778da16b825cf56
AKToran <aktoran60@gmail.com>
2026-06-28 11:04:46 +0600

    password hashing with bcryptjs and prevented password from returning

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,24 +1,33 @@
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
+import bcrypt from "bcryptjs";
 
 const createUserInDB = async (payload: IUser) => {
   const { name, email, password, age } = payload;
+
+  // const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, 10);
+
   const result = await pool.query(
     `
     INSERT INTO users(name, email, password, age) 
     VALUES($1,$2,$3,$4)
-    RETURNING *
+    RETURNING * 
     `,
-    [name, email, password, age],
+    [name, email, hashPassword, age],
   );
-
+  delete result.rows[0].password;
   return result;
 };
 
 const getAllUsersFromDB = async () => {
   const result = await pool.query(`
       SELECT * FROM USERS
-      `);
+  `);
+  
+  result.rows.map((row)=>{
+    delete row.password;
+  })
   return result;
 };
 
@@ -26,9 +35,12 @@ const getSingleUserFromDB = async (id : string) =>{
   const result = await pool.query(
       `
       SELECT * FROM users WHERE id=$1
+      console.log("🚀 ~ getSingleUserFromDB ~ $1:", $1)
       `,
       [id],
     );
+
+  delete result.rows[0].password;
   return result;
 }
 
